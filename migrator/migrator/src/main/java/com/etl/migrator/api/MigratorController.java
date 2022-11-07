003519e5d0b9a8aa1b283ad50a75dd645abe4e90
Noe Jimenez <112001036+noeHexaware@users.noreply.github.com>
2022-11-07 15:14:05 -0500

    Initial commit

---
+++ migrator/migrator/src/main/java/com/etl/migrator/api/MigratorController.java
@@ -0,0 +1,40 @@
+package com.etl.migrator.api;
+
+import com.etl.migrator.service.MigratorService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+@RestController
+@RequestMapping("api/v1")
+@RequiredArgsConstructor
+public class MigratorController {
+
+    private MigratorService migratorService;
+    
+    public MigratorController() throws SQLException{
+    	this.migratorService = new MigratorService();
+    }
+	
+    @GetMapping("hello")
+    public String hello(){
+        return "hello " + LocalDate.now();
+    }
+
+    @GetMapping("process")
+    public ResponseEntity<Object> processMigration() throws SQLException{
+    	//MigratorService migratorService = new MigratorService();
+        return ResponseEntity.ok(migratorService.processMigration());
+    }
+
+    @GetMapping("getCollection")
+    public ResponseEntity<Object> processCollection() throws SQLException {
+    	//MigratorService migratorService = new MigratorService();
+        return ResponseEntity.ok(migratorService.getCollection("department","idDepartment", "employee","department"));
+    }
+}
