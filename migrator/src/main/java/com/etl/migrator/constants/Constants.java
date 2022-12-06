3330bd581d0bece5ed11e9674ef1cc6a32adb05d
FerLizola <ferlizola95@gmail.com>
2022-12-06 14:37:41 -0500

    merged data

--- migrator/src/main/java/com/etl/migrator/constants/Constants.java
+++ migrator/src/main/java/com/etl/migrator/constants/Constants.java
@@ -4,5 +4,5 @@ public class Constants {
     public static String DATASOURCE_URL = "jdbc:mysql://localhost:3306/";
     public static String USERNAME = "root";
     public static String PASSWORD = "developer345";
-    public static String DATASOURCE_MONGODB = "mongodb+srv://ndev:RICT3qGl9KLhsyA8@cluster0.w8sdpyb.mongodb.net/?retryWrites=true&w=majority";
+    public static String DATASOURCE_MONGODB = "mongodb://root:example@localhost:27017";
 }
